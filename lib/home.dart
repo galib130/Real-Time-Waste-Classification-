cebdc00d22e702bee0cc075d42ca1d7f04e31b0e
galib130 <galibshams16@gmail.com>
2023-03-08 23:52:20 +0600

    added Camera Controller

---
+++ lib/home.dart
@@ -0,0 +1,37 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'main.dart';
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  CameraImage? cameraImage;
+  CameraController? cameraController;
+  String output='';
+
+  loadCamera(){
+    cameraController=CameraController( camera![0], ResolutionPreset.medium);
+    cameraController!.initialize().then((value){
+      if(!mounted){
+        return; 
+      }
+      else{
+
+        
+
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+      title: const Text('Everyday Object Detection') ),
+      );
+  }
+}
